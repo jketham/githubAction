2325d243d60f1f9d86e419033e27e0e6c7682fbf
jketham <50819932+jketham@users.noreply.github.com>
2024-07-22 19:14:23 +0530

    Update GithubActionApplication.java

--- src/main/java/com/example/githubAction/GithubActionApplication.java
+++ src/main/java/com/example/githubAction/GithubActionApplication.java
@@ -8,7 +8,7 @@ public class GithubActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionApplication.class, args);
-		System.out.println("hello Jana");
+		System.out.println("hello Jana Reddy");
 	}
 
 }
