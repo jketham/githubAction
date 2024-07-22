d78b198da0ddd9f68bcb6ca2721bcb29533058f8
jketham <jketham@gmail.com>
2024-07-22 19:05:09 +0530

    first commit

---
+++ src/main/java/com/example/githubAction/GithubActionApplication.java
@@ -0,0 +1,14 @@
+package com.example.githubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionApplication.class, args);
+		System.out.println("hello Jana");
+	}
+
+}
